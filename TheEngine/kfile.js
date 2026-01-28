fd0961eeb5dbb2bce684de0f5884182880968289
Tharle Josefi <tharlejc@gmail.com>
2026-01-28 14:07:21 -0500

    Add Separation between Game and Engine

--- TheEngine/kfile.js
+++ TheEngine/kfile.js
@@ -1,5 +1,58 @@
 const path = require('path');
 const fs = require('fs');
+const { hasSubscribers } = require('diagnostics_channel');
+const { error } = require('console');
+
+
+// Fist of all, re sync Assets 
+//Clean all Assets in Deployment
+emptyDirectory("./Deployment/Assets")
+
+//Verify if MyGame/Assets exist and create otherelse 
+checkMyGameAssets()
+
+//Re-sync all Assets to project
+try{
+    fs.cpSync("./MyGame/Assets", "./Deployment/Assets", {recursive : true, force: true});
+    console.log('All files and subdirectories copied successfully!');
+}catch(err){
+    console.error('Error copying Assets files:', err);
+}
+
+function emptyDirectory(dirPath) {
+    hasDirectory = fs.existsSync(dirPath);
+
+    if(hasDirectory) {
+        console.log('Directory Assets exists!');
+
+        try {
+            const files = fs.readdirSync(dirPath);
+
+            for (const file of files) {
+                const filePath = path.join(dirPath, file);
+                // Use fs.rmSync() with recursive: true to handle files and directories
+                fs.rmSync(filePath, { recursive: true, force: true }); 
+            }
+            console.log(`All files in ${dirPath} deleted synchronously.`);
+        } catch (error) {
+            console.error(`Error deleting files synchronously: ${error.message}`);
+        }
+    }else {
+        console.log('Directory Assets not found.');
+        fs.mkdirSync(dirPath, { recursive: true });
+        console.log('Directory Assets created successfully!');
+    }
+}
+
+function checkMyGameAssets() {
+    if(!fs.existsSync("./MyGame/Assets")){
+        fs.mkdirSync("./MyGame/Assets", { recursive: true });
+        console.log('Directory Game Assets created successfully!');
+    }
+}
+
+
+// Then create project
 let project = new Project('engine');
  
 project.addProvider = function(proj, isRoot=false){
@@ -24,6 +77,7 @@ project.addProvider = function(proj, isRoot=false){
             fs.copyFileSync("./SDL/lib/SDL2_mixer.dll", "./Deployment/SDL2_mixer.dll")
         }
     }
+
 };
 project.kore = false;
 
